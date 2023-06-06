3d5cf153159f5d36d9285122cfd2950cb9f0e3b9
valioprea <aeroprea@gmail.com>
2023-06-06 17:35:18 +0300

    first commit

---
+++ src/test/java/VehicleTest.java
@@ -0,0 +1,30 @@
+import org.example.Vehicle;
+import org.junit.jupiter.api.Test;
+import static org.junit.jupiter.api.Assertions.assertEquals;
+public class VehicleTest {
+
+    Vehicle vehicle;
+
+    @Test
+    public void go_increaseOdometerBy100AndDecreaseGallonsOfGasBy5WhenMilesPerGallonIs20(){
+        givenVehicleHas0Odometer15GallonsOfGasAnd20MilesPerHour();
+        whenDrive100();
+        thenOdometerIncreasesBy100AndGallonsOfGasDecreaseBy5();
+    }
+
+    private void givenVehicleHas0Odometer15GallonsOfGasAnd20MilesPerHour() {
+        vehicle = new Vehicle();
+        vehicle.setGallonsOfGas(15);
+        vehicle.setOdometer(0);
+        vehicle.setMilesPerGallon(20);
+    }
+
+    private void whenDrive100(){
+        vehicle.go(100);
+    }
+
+    private void thenOdometerIncreasesBy100AndGallonsOfGasDecreaseBy5() {
+        assertEquals(10, vehicle.getGallonsOfGas());
+        assertEquals(105, vehicle.getOdometer());
+    }
+}
